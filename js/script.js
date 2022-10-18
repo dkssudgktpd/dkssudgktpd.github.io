e2bc5069c7ceabb40c5804c39637c233dde6c7e0
moon <anstnqls554@gmail.com>
2022-10-18 18:02:02 +0900

    반응형 진행중

--- js/script.js
+++ js/script.js
@@ -1,56 +1,84 @@
-window.onload = function(){
+window.onload = function () {
   AOS.init();
-  const section = $('section');
-  const sectionH = []
-  $.each(section,function(index){
-    const windowH = Math.ceil($(this).offset().top-70)
-    sectionH[index] = windowH
-    new Waypoint({
-      element : $(this),
-      handler : function(direction){
-        if(direction == 'down'){
-          sectionBt.removeClass('active')
-          sectionBt.eq(index).addClass('active')
-        }
-      },
-      offset : '30%'
-    })
-    new Waypoint({
-      element : $(this),
-      handler : function(direction){
-        if(direction == 'up'){
-          sectionBt.removeClass('active')
-          sectionBt.eq(index).addClass('active')
-        }
-      },
-      offset : '-70%'
-    })
+  // 모바일 관련
+  const mbBt = $(".mb-bt");
+  const mbWrap = $(".mb-wrap");
+  const mbCon = $(".mb-content");
+  mbBt.click(function () {
+    mbBt.toggleClass("active");
+    if (mbBt.hasClass("active")) {
+      mbWrap.show();
+      $("html").css("overflow", "hidden");
+    } else {
+      mbWrap.hide();
+      $("html").css("overflow", "auto");
+    }
+  });
+  mbWrap.click(function () {
+    mbWrap.hide();
+    mbBt.removeClass("active");
+  });
+  mbCon.click(function (e) {
+    e.stopPropagation();
+  });
+  $(window).resize(function () {
+    let temp = $(window).width();
+    if (temp > 800) {
+      mbBt.removeClass("active");
+      mbWrap.hide();
+    }
   })
-  const sectionBt = $('.section-bt');
-  $.each(sectionBt,function(index){
-    $(this).click(function(event){
+  // 섹션 액티브 관련
+  const section = $("section");
+  const sectionH = [];
+  $.each(section, function (index) {
+    const windowH = Math.ceil($(this).offset().top - 70);
+    sectionH[index] = windowH;
+    new Waypoint({
+      element: $(this),
+      handler: function (direction) {
+        if (direction == "down") {
+          sectionBt.removeClass("active");
+          sectionBt.eq(index).addClass("active");
+        }
+      },
+      offset: "30%",
+    });
+    new Waypoint({
+      element: $(this),
+      handler: function (direction) {
+        if (direction == "up") {
+          sectionBt.removeClass("active");
+          sectionBt.eq(index).addClass("active");
+        }
+      },
+      offset: "-70%",
+    });
+  });
+  const sectionBt = $(".section-bt");
+  $.each(sectionBt, function (index) {
+    $(this).click(function (event) {
       event.preventDefault();
       scrollTo({
-        top : sectionH[index],
-        left : 0,
-        behavior : 'smooth'
-      })
-    })
-  })
-  const project = $('.project');
-  const visualBt = $('.visual-bt');
+        top: sectionH[index],
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  });
+  const project = $(".project");
+  const visualBt = $(".visual-bt");
   visualBt.click(() => {
-    
     scrollTo({
-      top : project.offset().top-70,
-      behavior: "smooth"
-    })
-  })
-  const gotop = $('.gotop');
-  gotop.click(function(){
+      top: project.offset().top - 70,
+      behavior: "smooth",
+    });
+  });
+  const gotop = $(".gotop");
+  gotop.click(function () {
     scrollTo({
-      top:0,
-      behavior:'smooth'
-    })
-  })
-}
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+};
